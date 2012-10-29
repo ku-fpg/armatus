b134f7e94bb5904d60eda2c343db43dee12d8def
xnux <xnux@ittc.ku.edu>
2012-10-28 21:55:46 -0500

    Allowed the text in the ListView to be edited

--- Android HERMIT/src/com/kufpg/androidhermit/WarpDSLV.java
+++ Android HERMIT/src/com/kufpg/androidhermit/WarpDSLV.java
@@ -2,89 +2,95 @@ package com.kufpg.androidhermit;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.kufpg.androidhermit.dragsort.DragSortListView;
 import com.kufpg.androidhermit.util.FileIOManager;
 
-
 import android.app.ActionBar;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 public class WarpDSLV extends ListActivity {
 
 	public final static int FILE_FROM_DISK = 1;
-    private ArrayAdapter<String> adapter;
-    private ArrayList<String> list;
+	private CodeAdapter mAdapter;
+	private ArrayList<String> mList;
 
-    private DragSortListView.DropListener onDrop =
-        new DragSortListView.DropListener() {
-            @Override
-            public void drop(int from, int to) {
-                String item=adapter.getItem(from);
+	private DragSortListView.DropListener onDrop =
+			new DragSortListView.DropListener() {
+		@Override
+		public void drop(int from, int to) {
+			String item=mAdapter.getItem(from);
 
-                adapter.notifyDataSetChanged();
-                adapter.remove(item);
-                adapter.insert(item, to);
-            }
-        };
+			mAdapter.notifyDataSetChanged();
+			mAdapter.remove(item);
+			mAdapter.insert(item, to);
+		}
+	};
 
-    private DragSortListView.RemoveListener onRemove = 
-        new DragSortListView.RemoveListener() {
-            @Override
-            public void remove(int which) {
-                adapter.remove(adapter.getItem(which));
-            }
-        };
+	private DragSortListView.RemoveListener onRemove = 
+			new DragSortListView.RemoveListener() {
+		@Override
+		public void remove(int which) {
+			mAdapter.remove(mAdapter.getItem(which));
+		}
+	};
 
-    private DragSortListView.DragScrollProfile ssProfile =
-        new DragSortListView.DragScrollProfile() {
-            @Override
-            public float getSpeed(float w, long t) {
-                if (w > 0.8f) {
-                    // Traverse all views in a millisecond
-                    return ((float) adapter.getCount()) / 0.001f;
-                } else {
-                    return 10.0f * w;
-                }
-            }
-        };
+	private DragSortListView.DragScrollProfile ssProfile =
+			new DragSortListView.DragScrollProfile() {
+		@Override
+		public float getSpeed(float w, long t) {
+			if (w > 0.8f) {
+				// Traverse all views in a millisecond
+				return ((float) mAdapter.getCount()) / 0.001f;
+			} else {
+				return 10.0f * w;
+			}
+		}
+	};
 
-    /** Called when the activity is first created. */
-    @SuppressWarnings("unchecked")
+	/** Called when the activity is first created. */
+	@SuppressWarnings("unchecked")
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.warp_main);
-        ActionBar actionBar = getActionBar();
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.warp_main);
+		ActionBar actionBar = getActionBar();
 		actionBar.show();
 
-        DragSortListView lv = (DragSortListView) getListView(); 
+		DragSortListView lv = (DragSortListView) getListView(); 
 
-        lv.setDropListener(onDrop);
-        lv.setRemoveListener(onRemove);
-        lv.setDragScrollProfile(ssProfile);
+		lv.setDropListener(onDrop);
+		lv.setRemoveListener(onRemove);
+		lv.setDragScrollProfile(ssProfile);
 
-        list = (ArrayList<String>) getIntent().getSerializableExtra("CODE_ARRAY");
+		mList = (ArrayList<String>) getIntent().getSerializableExtra("CODE_ARRAY");
 
-        adapter = new ArrayAdapter<String>(this, R.layout.list_item_handle_right, R.id.text, list);
-        setListAdapter(adapter);
-    }
-    
-    @Override
+		mAdapter = new CodeAdapter(mList);
+		setListAdapter(mAdapter);
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.actionbar, menu);
 		return true;
 	}
-    
-    @Override
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item){
 		switch(item.getItemId()) {
 		case R.id.open_file:
@@ -103,8 +109,8 @@ public class WarpDSLV extends ListActivity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-    
-    @Override
+
+	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent intent) { 
 		super.onActivityResult(requestCode, resultCode, intent);
 
@@ -124,6 +130,62 @@ public class WarpDSLV extends ListActivity {
 				finish();
 				startActivity(codeIntent);
 			}
+		}
+	}
+
+	private class ViewHolder {
+		public EditText codeView;
+	}
+
+	class ListItem {
+		String lineOfCode;
+	}
+
+	private class CodeAdapter extends ArrayAdapter<String> {
+		private LayoutInflater mInflater;
+		private ArrayList<String> backupItems = new ArrayList<String>();
+
+		public CodeAdapter(List<String> codeLines) {
+			super(WarpDSLV.this, R.layout.list_item_handle_right,
+					R.id.text, codeLines);
+			mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			backupItems = mList;
+			notifyDataSetChanged();
+		}
+
+		public int getCount() {
+			return backupItems.size();
+		}
+
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+
+			if (convertView == null) {
+				holder = new ViewHolder();
+				convertView = mInflater.inflate(R.layout.list_item_handle_right, null);
+				holder.codeView = (EditText) convertView.findViewById(R.id.text);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			
+			//Fill EditText with the value you have in data source
+			holder.codeView.setText(backupItems.get(position));
+			holder.codeView.setId(position);
+
+			//we need to update adapter once we finish with editing
+			holder.codeView.setOnFocusChangeListener(new OnFocusChangeListener() {
+				public void onFocusChange(View v, boolean hasFocus) {
+					if (!hasFocus){
+						final int position = v.getId();
+						final EditText Caption = (EditText) v;
+						backupItems.set(position, Caption.getText().toString());
+					}
+				}
+			});
+
+			return convertView;
 		}
 	}
 
