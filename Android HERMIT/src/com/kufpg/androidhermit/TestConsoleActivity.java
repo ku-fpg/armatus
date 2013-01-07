0d0f2c8e7c58ae8bde054636012bfa2f93479401
tucker19 <adam.dhowell.19@gmail.com>
2013-01-07 15:34:32 -0600

    Got keyboard working and input box on the bottom of screen. Does not auto scroll now but will work on it.

--- Android HERMIT/src/com/kufpg/androidhermit/TestConsoleActivity.java
+++ Android HERMIT/src/com/kufpg/androidhermit/TestConsoleActivity.java
@@ -2,8 +2,10 @@ package com.kufpg.androidhermit;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.ScrollView;
@@ -17,6 +19,7 @@ public class TestConsoleActivity extends StandardActivity {
 	private View recent;
 	private LayoutParams lp;
 	private ScrollView sv;
+	private EditText et;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,6 +27,21 @@ public class TestConsoleActivity extends StandardActivity {
 
 		sv = (ScrollView) findViewById(R.id.scroll);
 		rr = (RelativeLayout) findViewById(R.id.whatever);
+		et = (EditText) findViewById(R.id.inputBox);
+		et.setOnKeyListener(new EditText.OnKeyListener() {
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (keyCode == KeyEvent.KEYCODE_ENTER
+						&& event.getAction() == KeyEvent.ACTION_UP) {
+
+					addMessage("Time: " + System.currentTimeMillis());
+					return true;
+				}
+				return false;
+			}
+
+		});
+		
 		b1 = (Button) findViewById(R.id.add_text_button);
 		recent = b1;
 		b1.setText("Click me");
@@ -31,27 +49,26 @@ public class TestConsoleActivity extends StandardActivity {
 		b1.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				tv1 = new TextView(TestConsoleActivity.this);
-				tv1.setGravity(Gravity.BOTTOM);
-				tv1.setId((int) System.currentTimeMillis());
-				lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
-						LayoutParams.WRAP_CONTENT);
-				lp.addRule(RelativeLayout.BELOW, recent.getId());
-				addMessage(tv1, "Time: " + System.currentTimeMillis());
-				recent = tv1;
+				addMessage("Time: " + System.currentTimeMillis());
 			}
 		});
 	}
 
-	private void addMessage(final TextView tv, String msg) {
-		// append the new string
-		tv.setText(msg);
-		rr.addView(tv, lp);
+	private void addMessage(String msg) {
+		tv1 = new TextView(TestConsoleActivity.this);
+		tv1.setGravity(Gravity.BOTTOM);
+		tv1.setId((int) System.currentTimeMillis());
+		lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
+		lp.addRule(RelativeLayout.BELOW, recent.getId());
+		tv1.setText(msg);
+		rr.addView(tv1, lp);
 		sv.post(new Runnable() {
 			public void run() {
-				sv.smoothScrollTo(0, tv.getBottom());
+				sv.smoothScrollTo(0, tv1.getBottom());
 			}
 		});
+		recent = tv1;
 	}
 
 }
