6666f46e9f447d7b3e0c031e971559f0a688aace
tucker19 <adam.dhowell.19@gmail.com>
2013-01-27 13:50:13 -0600

    Added context menu, recognizes the color commands for now. Takes long click to bring context menu up.

--- Android HERMIT/src/com/kufpg/androidhermit/ConsoleActivity.java
+++ Android HERMIT/src/com/kufpg/androidhermit/ConsoleActivity.java
@@ -1,6 +1,8 @@
 package com.kufpg.androidhermit;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
@@ -10,9 +12,15 @@ import com.kufpg.androidhermit.util.ConsoleTextView;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnLongClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.ScrollView;
@@ -31,6 +39,11 @@ public class ConsoleActivity extends StandardActivity {
 	private LinkedHashMap<Integer, ConsoleTextView> mCommandHistory = new LinkedHashMap<Integer, ConsoleTextView>();
 	private int mCommandCount = 0;
 	private CommandDispatcher mDispatcher;
+	private static final String RED = "red";
+	private static final String BLUE = "blue";
+	private static final String GREEN = "green";
+	private HashMap<String,String> mContextMenuMap = new HashMap<String, String> ();
+	private ArrayList<String> mValues = new ArrayList<String>();
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -42,6 +55,9 @@ public class ConsoleActivity extends StandardActivity {
 		mInputHeader = (TextView) findViewById(R.id.code_command_num);
 		mInputHeader.setText("hermit<" + mCommandCount + "> ");
 		mInputEditText = (EditText) findViewById(R.id.code_input_box);
+		mContextMenuMap.put(BLUE, BLUE);
+		mContextMenuMap.put(RED, RED);
+		mContextMenuMap.put(GREEN, GREEN);
 		mInputEditText.setOnKeyListener(new EditText.OnKeyListener() {
 			@Override
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -95,6 +111,29 @@ public class ConsoleActivity extends StandardActivity {
 		mCommandHistory = (LinkedHashMap<Integer, ConsoleTextView>) savedInstanceState.getSerializable("CmdHistory");
 		refreshConsole(mCommandHistory);
 	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+	{
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.setHeaderTitle(R.string.Context_Menu_Title);
+		int order = 0;
+		for(String value: mValues)
+		{
+			menu.add(0, v.getId(), order, value);
+			order++;
+		}
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item)
+	{
+		if(item != null)
+		{
+			makeToast(item.getTitle().toString());
+		}
+		return super.onContextItemSelected(item);
+	}
 
 	public void clear() {
 		mCodeLayout.removeAllViews();
@@ -115,6 +154,43 @@ public class ConsoleActivity extends StandardActivity {
 	 */
 	public void addMessage(String msg) {
 		mCurConsoleTextView = new ConsoleTextView(ConsoleActivity.this, msg, mCommandCount);
+		registerForContextMenu(mCurConsoleTextView);
+		mCurConsoleTextView.setOnLongClickListener(new OnLongClickListener() {
+
+			@Override
+			public boolean onLongClick(View v) {
+				
+				mValues.clear();
+				String [] mArry = mCurConsoleTextView.getText().toString().split(" ");
+				
+				for(int i = 1; i < mArry.length; i++)
+				{
+					if(mArry[i].equals(mContextMenuMap.get(BLUE)))
+					{
+						mValues.add("Blue");
+					}
+					else if(mArry[i].equals(mContextMenuMap.get(GREEN)))
+					{
+						mValues.add("Green");
+					}
+					else if(mArry[i].equals(mContextMenuMap.get(RED)))
+					{
+						mValues.add("Red");
+					}
+						
+				}
+				
+				
+				if(!mValues.isEmpty())
+				{
+					ConsoleActivity.this.openContextMenu(mCurConsoleTextView);
+					return true;
+				}
+				
+				return false;
+			}
+			
+		});
 		mCommandHistory.put(mCurConsoleTextView.getId(), mCurConsoleTextView);
 		mCommandCount++;
 
