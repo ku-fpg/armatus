ebd56c0013e947e5bec3dfbc17a51f2571d17542
xnux <xnux@ittc.ku.edu>
2013-01-29 15:26:30 -0500

    Pinch-zoom text resizing is functional

--- Android HERMIT/src/com/kufpg/androidhermit/TextSizePinchZoomActivity.java
+++ Android HERMIT/src/com/kufpg/androidhermit/TextSizePinchZoomActivity.java
@@ -1,63 +1,87 @@
 package com.kufpg.androidhermit;
 
-import com.kufpg.androidhermit.util.ConsoleTextView;
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
-import android.util.TypedValue;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class TextSizePinchZoomActivity extends Activity {
 
-    TextView scaleGesture;
-    ScaleGestureDetector scaleGestureDetector;
-    private final static int DEFAULT_FONT_SIZE = ConsoleActivity.DEFAULT_FONT_SIZE;
+	private final static int DEFAULT_FONT_SIZE = ConsoleActivity.DEFAULT_FONT_SIZE;
 	private final static int MAX_FONT_SIZE = ConsoleActivity.MAX_FONT_SIZE;
 	private final static int MIN_FONT_SIZE = ConsoleActivity.MIN_FONT_SIZE;
+	
+	private TextView mScalingView;
+	private ScaleGestureDetector mScaleGestureDetector;
+	private PopupWindow mPopup;
+	private View mPopupView;
+	private TextView mPopupTextView;
+	private int mIntFontSize = DEFAULT_FONT_SIZE;
+	private float mFloatFontSize = DEFAULT_FONT_SIZE;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.textsize_pinchzoom);
-        scaleGesture = (TextView)findViewById(R.id.resize_me);
-        scaleGesture.setText("this is some text");
-        scaleGesture.setTextSize(DEFAULT_FONT_SIZE);
-        scaleGestureDetector = new ScaleGestureDetector(this, new simpleOnScaleGestureListener());
-    }
-
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        // TODO Auto-generated method stub
-        scaleGestureDetector.onTouchEvent(event);
-        return true;
-    }
-
-    public class simpleOnScaleGestureListener extends
-            SimpleOnScaleGestureListener {
-
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            // TODO Auto-generated method stub
-            float size = scaleGesture.getTextSize();
-            Log.d("TextSizeStart", String.valueOf(size));
-
-            float factor = detector.getScaleFactor();
-            Log.d("Factor", String.valueOf(factor));
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.textsize_pinchzoom);
+		mScalingView = (TextView) findViewById(R.id.resize_me);
+		mScalingView.setTextSize(DEFAULT_FONT_SIZE);
+		mScaleGestureDetector = new ScaleGestureDetector(this, new OnConsoleScaleGestureListener());
+		
+		LayoutInflater inflater = (LayoutInflater)
+				getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mPopupView = inflater.inflate(R.layout.popup,
+                (ViewGroup) findViewById(R.id.popup_layout));
+		mPopupView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+		mPopupTextView = (TextView) mPopupView.findViewById(R.id.popup_textview);
+		refreshPopup(DEFAULT_FONT_SIZE);
+		mPopup = new PopupWindow(mPopupView, ViewGroup.LayoutParams.WRAP_CONTENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT, false);
+	}
 
 
-            float product = size*factor;
-            product = Math.max(MIN_FONT_SIZE, Math.min(product, MAX_FONT_SIZE));
-            Log.d("TextSize", String.valueOf(product));
-            scaleGesture.setTextSize(TypedValue.COMPLEX_UNIT_PX, product);
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (mScaleGestureDetector.onTouchEvent(event)) {
+			if ((event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE)
+					&& mScaleGestureDetector.isInProgress()) {
+				mPopup.showAtLocation(mPopupView, Gravity.CENTER, 0, 0);
+				return true;
+			} else if (event.getAction() == MotionEvent.ACTION_UP) {
+				mScalingView.setTextSize(mIntFontSize);
+				mPopup.dismiss();
+				return false;
+			}
+		}
+		return super.onTouchEvent(event);
+	}
 
-            size = scaleGesture.getTextSize();
-            Log.d("TextSizeEnd", String.valueOf(size));
-            return true;
-        }
-    }
+	public class OnConsoleScaleGestureListener extends SimpleOnScaleGestureListener {
+		@Override
+		public boolean onScale(ScaleGestureDetector detector) {
+			//float size = scaleGesture.getTextSize();
+			float size = mFloatFontSize;
+			float factor = detector.getScaleFactor();
+			float product = size*factor;
+			product = Math.max(MIN_FONT_SIZE, Math.min(product, MAX_FONT_SIZE));
+			//scaleGesture.setTextSize(product);
+			//size = scaleGesture.getTextSize();
+			refreshPopup((int) product);
+			mFloatFontSize = product;
+			return true;
+		}
+	}
+	
+	private void refreshPopup(int newFontSize) {
+		mIntFontSize = newFontSize;
+		String label = getResources().getString(R.string.popup_text);
+		mPopupTextView.setText(label + " " + newFontSize);
+	}
 }
