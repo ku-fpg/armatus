cf4a61fe6700e8b973c75ee3a1a1f7be050312e9
RyanGlScott <ryan.gl.scott@ku.edu>
2013-09-09 17:44:51 -0500

    Made Glyph match newest API version

--- Android/Armatus Android App/src/edu/kufpg/armatus/console/HermitClient.java
+++ Android/Armatus Android App/src/edu/kufpg/armatus/console/HermitClient.java
@@ -210,15 +210,15 @@ public class HermitClient implements Parcelable {
 		}
 
 		private static List<Glyph> jsonToGlyphs(JSONArray a) {
-			List<Glyph> list = new ArrayList<Glyph>();
+			ImmutableList.Builder<Glyph> builder = ImmutableList.builder();
 			for (int i = 0; i < a.length(); i++) {
 				try {
-					list.add(new Glyph(a.getJSONObject(i)));
+					builder.add(new Glyph(a.getJSONObject(i)));
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}
 			}
-			return list;
+			return builder.build();
 		}
 	}
 
@@ -226,23 +226,41 @@ public class HermitClient implements Parcelable {
 		private static final long serialVersionUID = -7814082698884658726L;
 
 		private final GlyphStyle mStyle;
+		private final List<Crumb> mPath;
 		private final String mText;
 
-		public Glyph(JSONObject o) throws JSONException {
-			this(jsonToStyle(o), o.getString("text"));
-		}
-
-		public Glyph(GlyphStyle style, String text) {
+		public Glyph(GlyphStyle style, List<Crumb> path, String text) {
 			mStyle = style;
+			mPath = path;
 			mText = text;
+		}
+		
+		public Glyph(JSONObject o) throws JSONException {
+			this(jsonToStyle(o), jsonToCrumbs(o.getJSONArray("path")), o.getString("text"));
 		}
 
 		public GlyphStyle getStyle() {
 			return mStyle;
 		}
+		
+		public List<Crumb> getPath() {
+			return mPath;
+		}
 
 		public String getText() {
 			return mText;
+		}
+		
+		private static List<Crumb> jsonToCrumbs(JSONArray a) {
+			ImmutableList.Builder<Crumb> builder = ImmutableList.builder();
+			for (int i = 0; i < a.length(); i++) {
+				try {
+					builder.add(new Crumb(a.getJSONObject(i)));
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+			return builder.build();
 		}
 
 		private static GlyphStyle jsonToStyle(JSONObject o) {
@@ -265,6 +283,41 @@ public class HermitClient implements Parcelable {
 
 	public static enum GlyphStyle {
 		NORMAL, KEYWORD, SYNTAX, VAR, COERCION, TYPE, LIT, WARNING
+	}
+	
+	public static class Crumb implements Serializable {
+		private static final long serialVersionUID = 2053543270858395622L;
+		private final int mNum;
+		private final String mCrumb;
+		
+		public Crumb(int num, String crumb) {
+			mNum = num;
+			mCrumb = crumb;
+		}
+		
+		public Crumb(JSONObject o) throws JSONException {
+			this(jsonToNum(o), o.getString("crumb"));
+		}
+		
+		public int getNum() {
+			return mNum;
+		}
+		
+		public String getCrumb() {
+			return mCrumb;
+		}
+		
+		private static int jsonToNum(JSONObject o) {
+			int num = -1;
+			if (o.has("num")) {
+				try {
+					num = o.getInt("style");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+			return num;
+		}
 	}
 
 	public static class Token implements Serializable {
