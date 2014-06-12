d5aa1a34e8525f4f8a04dbf03a1dcc9d94bc073b
RyanGlScott <ryan.gl.scott@ku.edu>
2014-06-12 13:50:41 -0500

    Give up on Scala support for now

--- Armatus Android App/src/edu/kufpg/armatus/util/Views.java
+++ Armatus Android App/src/edu/kufpg/armatus/util/Views.java
@@ -1,34 +1,37 @@
 package edu.kufpg.armatus.util;
 
+import android.support.annotation.NonNull;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
 
 public final class Views {
-	
-	private Views() {}
-	
-	/**
-	 * Returns whether a particular ListView entry is currently shown to the
-	 * user on-screen.
-	 * @param listView
-	 * @param entryIndex The index of the entry to look up.
-	 * @return {@code true} if the entry is currently visible to the user.
-	 */
-	public static boolean isEntryVisible(ListView listView, int entryIndex) {
-		return listView.getFirstVisiblePosition() <= entryIndex
-				&& entryIndex <= listView.getLastVisiblePosition();
-	}
-	
-	public static int getFlatListPosition(ExpandableListView expListView, int groupPos, int childPos) {
-		return expListView.getFlatListPosition(ExpandableListView.getPackedPositionForChild(groupPos, childPos));
-	}
-	
-	public static int getGroupPosition(ExpandableListView expListView, int flatListPos) {
-		return ExpandableListView.getPackedPositionGroup(expListView.getExpandableListPosition(flatListPos));
-	}
-	
-	public static int getChildPosition(ExpandableListView expListView, int flatListPos) {
-		return ExpandableListView.getPackedPositionChild(expListView.getExpandableListPosition(flatListPos));
-	}
-	
+
+    private Views() {
+    }
+
+    /**
+     * Returns whether a particular ListView entry is currently shown to the
+     * user on-screen.
+     *
+     * @param listView
+     * @param entryIndex The index of the entry to look up.
+     * @return {@code true} if the entry is currently visible to the user.
+     */
+    public static boolean isEntryVisible(@NonNull final ListView listView, final int entryIndex) {
+        return listView.getFirstVisiblePosition() <= entryIndex
+                && entryIndex <= listView.getLastVisiblePosition();
+    }
+
+    public static int getFlatListPosition(@NonNull final ExpandableListView expListView, final int groupPos, final int childPos) {
+        return expListView.getFlatListPosition(ExpandableListView.getPackedPositionForChild(groupPos, childPos));
+    }
+
+    public static int getGroupPosition(@NonNull final ExpandableListView expListView, final int flatListPos) {
+        return ExpandableListView.getPackedPositionGroup(expListView.getExpandableListPosition(flatListPos));
+    }
+
+    public static int getChildPosition(@NonNull final ExpandableListView expListView, final int flatListPos) {
+        return ExpandableListView.getPackedPositionChild(expListView.getExpandableListPosition(flatListPos));
+    }
+
 }
